fa628a9e6048a2a35c49be50928824d7eed71fe3
kuri <leavemealone@example.com>
2020-10-21 18:16:18 +0200

    started working on choropleth map

--- swisscovid.py
+++ swisscovid.py
@@ -13,59 +13,21 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "Ira Kurthen"
 __contact__ = "ira.kurthen[at]gmail.com"
-__date__ = "2020/10/03"
+__date__ = "2020/10/21"
 __deprecated__ = False
 __license__ = "GPLv3"
-__version__ = "0.0.3"
+__version__ = "0.1.0"
 
 import numpy as np
 import pandas as pd
-#import datetime
+import datetime
 #import matplotlib.pyplot as plt
 import streamlit as st
 import plotly.graph_objects as go
-
-@st.cache
-def load_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total.csv', 
-                     parse_dates = ["date"])
-    return df
-
-df = load_data()
-#print(df)
-# df = pd.read_csv('D:\Programming\swiss-covid-app\COVID19_Fallzahlen_CH_total.csv')
-
-st.title("Covid-19 Dashboard for Switzerland")
-st.markdown("This is a dashboard made for exploring how the Swiss cantons "
-            "differ in Covid-19 caseload. It has been programmed by " 
-            "[Ira Kurthen](https://datamahou.ch/cv/) " 
-            "and it uses open data provided by " 
-            "[Statistisches Amt Kanton Zürich](https://github.com/openZH/covid_19).")
-st.markdown("The code for this dashboard can be found on my [GitHub](https://github.com/kuri-majo/swiss-covid-app). "
-            "The dashboard was programmed using [Streamlit](https://www.streamlit.io/) "
-            "and is hosted on an SSL-secured [DigitalOcean](https://www.digitalocean.com/) droplet "
-            "served by [nginx](https://www.nginx.com/).")
-st.markdown('To get started...')
-
-# inhabitants per canton
-@st.cache
-def load_data_cantons():
-    #df = pd.read_excel('https://www.bfs.admin.ch/bfsstatic/dam/assets/11587762/master/je-d-21.03.02.xlsx', skiprows = 3).rename(columns={"Unnamed: 0":"Indicator"}) # canton names are in row 4
-    df = pd.read_excel('je-d-21.03.02.xlsx', skiprows = 3).rename(columns={"Unnamed: 0":"Indicator"}) # canton names are in row 4
-    df = df[df["Indicator"].str.contains("Einwohner in 1000", na = False)]
-    df = df.transpose() # now, cantons are the row index
-    df = df.iloc[3:] # first three rows do not contain cantons
-    #df = df.div(1000) # first in 1000, now in 100000
-    df.columns = ['Inhabitants_1000']
-    return df
-
-inhabitants_cantons = load_data_cantons()
-
-# join inhabitant number info with covid data
-df = pd.merge(df, inhabitants_cantons, left_on = "abbreviation_canton_and_fl", right_index = True, 
-         how = "left")
-
-df["cases_per_1000_inhabitants"] = np.divide(df["ncumul_conf"], df["Inhabitants_1000"])
+import geopandas as gpd
+import json
+#import fiona
+#import os.path
 
 # canton abbreviations are used in the dataset, so we need to know these abbreviations
 canton_dict = {
@@ -95,6 +57,93 @@ canton_dict = {
 	"Neuchâtel": "NE", 
 	"Geneva": "GE", 
 	"Jura": "JU"}
+# also add a reversed version of the dictionary
+rev_canton_dict = {value : key for (key, value) in canton_dict.items()}
+
+@st.cache
+def load_data():
+    df = pd.read_csv('https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total.csv', 
+                     parse_dates = ["date"])
+    df['canton_full_name'] = df['abbreviation_canton_and_fl'].map(rev_canton_dict) # add full canton names
+    return df
+
+df = load_data()
+#print(df)
+# df = pd.read_csv('D:\Programming\swiss-covid-app\COVID19_Fallzahlen_CH_total.csv')
+
+# text in the beginning
+if 1:
+    st.title("Covid-19 Dashboard for Switzerland")
+    st.markdown("This is a dashboard made for exploring how the Swiss cantons "
+                "differ in Covid-19 caseload. It has been programmed by " 
+                "[Ira Kurthen](https://datamahou.ch/cv/) " 
+                "and it uses open data provided by " 
+                "[Statistisches Amt Kanton Zürich](https://github.com/openZH/covid_19).")
+    st.markdown("The code for this dashboard can be found on my [GitHub](https://github.com/kuri-majo/swiss-covid-app). "
+                "The dashboard was programmed using [Streamlit](https://www.streamlit.io/) "
+                "and is hosted on an SSL-secured [DigitalOcean](https://www.digitalocean.com/) droplet "
+                "served by [nginx](https://www.nginx.com/).")
+    st.markdown('To get started...')
+
+# inhabitants per canton
+@st.cache
+def load_data_cantons():
+    #df = pd.read_excel('https://www.bfs.admin.ch/bfsstatic/dam/assets/11587762/master/je-d-21.03.02.xlsx', skiprows = 3).rename(columns={"Unnamed: 0":"Indicator"}) # canton names are in row 4
+    df = pd.read_excel('je-d-21.03.02.xlsx', skiprows = 3).rename(columns={"Unnamed: 0":"Indicator"}) # canton names are in row 4
+    df = df[df["Indicator"].str.contains("Einwohner in 1000", na = False)]
+    df = df.transpose() # now, cantons are the row index
+    df = df.iloc[3:] # first three rows do not contain cantons
+    #df = df.div(1000) # first in 1000, now in 100000
+    df.columns = ['Inhabitants_1000']
+    return df
+
+inhabitants_cantons = load_data_cantons()
+
+# join inhabitant number info with covid data
+df = pd.merge(df, inhabitants_cantons, left_on = "abbreviation_canton_and_fl", right_index = True, 
+         how = "left")
+
+df["cases_per_1000_inhabitants"] = np.divide(df["ncumul_conf"], df["Inhabitants_1000"])
+
+map_cantons = gpd.read_file("D:\Programming\swiss-covid-app\CHE_adm\CHE_adm1.shp", 
+                            encoding = 'utf-8')
+
+#canton_and_ncumul_only = df[["canton_full_name", "ncumul_conf"]]
+
+#map_with_covid = map_cantons.merge(canton_and_ncumul_only, how = "left", left_on = "NAME_1", right_on = "canton_full_name")
+
+today = datetime.date.today()
+today2 = pd.to_datetime(today)
+today3 = str(today2)
+st.write("Today's date:", today2)
+ # - datetime.timedelta(days=1),
+
+# read shapefile and convert to geojson, necessary for plotly choropleth maps
+map_cantons.to_file("CHE_adm1.geojson", driver = "GeoJSON")
+with open("CHE_adm1.geojson") as geofile:
+        cantons_jsonfile = json.load(geofile)
+
+fig = go.Figure(data = go.Choroplethmapbox(
+    geojson = cantons_jsonfile, 
+    z = df["ncumul_conf"], 
+    #z = df.loc[df['date'] == today3, ['ncumul_conf']], 
+    locations = df["canton_full_name"], 
+    featureidkey='properties.NAME_1',
+    #featureidkey="properties.district",
+    #center = {"lat": 46.94809, "lon": 7.44744} 
+    ))
+
+fig.update_layout(title_text = 'Cases per Canton',
+                  title_x=0.5,
+                  coloraxis_colorscale='algae_r',
+                  mapbox=dict(style='carto-positron',
+                              zoom=6, 
+                              center = {"lat": 46.8181877 , "lon":8.2275124 },
+                              )); 
+
+st.write(fig)
+
+#cantons_jsonfile["features"][0]
 
 # heading canton selection
 st.header("Select the cantons you wish to compare.")
@@ -112,7 +161,6 @@ selected_cantons_df = df.loc[df['abbreviation_canton_and_fl'].isin(selected_cant
 #st.write(selected_cantons_df)
 
 # add possibility of switching to log y axis
-
 log_y_axis = st.selectbox(
     'Y-axis: linear or logarithmic?',
      ['linear', 'logarithmic'])
